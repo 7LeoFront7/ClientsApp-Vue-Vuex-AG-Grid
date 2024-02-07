50e002fcb903bb4f0661dcdabf2cd7216b728763
Maxim <xonskyfilms@gmail.com>
2024-02-07 18:16:51 +0300

    Инициализация проекта

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Table from './components/Table.vue'
+</script>
+
+<template>
+  <div class="w-11/12 mx-auto">
+    <Table />
+  </div>
+</template>
+
---
+++ src/components/Table.vue
@@ -0,0 +1,30 @@
+<script setup>
+import "ag-grid-community/styles/ag-grid.css" // Core CSS
+import "ag-grid-community/styles/ag-theme-quartz.css" // Theme
+import { AgGridVue } from "ag-grid-vue3" // Vue Grid Logic
+import { ref } from 'vue'
+
+
+
+const rowData = ref([
+  { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+  { make: "Ford", model: "F-Series", price: 33850, electric: false },
+  { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+])
+
+const colDefs = ref([
+  { field: "make" },
+  { field: "model" },
+  { field: "price" },
+  { field: "electric" }
+])
+
+
+
+
+</script>
+
+<template>
+  <ag-grid-vue style="height: 500px" class="ag-theme-quartz" :rowData="rowData" :columnDefs="colDefs">
+  </ag-grid-vue>
+</template>
